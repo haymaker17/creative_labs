bc203a7c56bdcc4e273f48f794148a183f5d59bd
Chris Wright <chris@wirescape.tv>
2014-08-26 14:34:00 -0700

    now using skrollr for the fadein elements

--- js/scripts.js
+++ js/scripts.js
@@ -10,53 +10,16 @@ function sendMailTo(name, company, domain) {
   window.location.replace(locationstring);
 }
 
-
-function fadeinElements() {
-    /* Check the location of each desired element */
-    $('.fadeinElement').each(function(i) {
-
-        var height_of_object = $(this).outerHeight();
-        var bottom_of_object = $(this).offset().top + height_of_object;
-        var middle_of_object = bottom_of_object - (height_of_object/2);
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-        /* If the object is completely visible in the window, fade it it */
-        if (bottom_of_window > middle_of_object) {
-
-            $(this).addClass("visible");
-
-        }
-        else {
-        	//$(this).removeClass("visible");
-        }
-
-    });
-}
-
-
-
 $(function(){
 
-	//parallax();
-	fadeinElements();
-
-	$(window).bind('scroll', function(){
-		//parallax();
-		fadeinElements();
-	});
+    $(".better_together .block").addClass("visible");
 
     // Init Skrollr
     skrollr.init({
         smoothScrolling: false,
-        // mobileDeceleration: 0.004,
+        mobileDeceleration: 0.004,
         forceHeight: false
     });
 
 
-
-
 })
-
-
-
-
